10715a20756857db24b89542fd92fc724a2257c3
Juliana Villegas <jumavipe@hotmail.com>
2018-11-20 11:37:21 -0500

    Update service-worker.js

--- mentech/service-worker.js
+++ mentech/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.481a76093db1cabebce1033b265383c0.js"
+  "https://booleanavillegas.github.io/mentech/precache-manifest.481a76093db1cabebce1033b265383c0.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("https://booleanavillegas.github.io/mentech/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
