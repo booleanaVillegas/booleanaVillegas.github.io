f979fb7feeb323128e2e35ebbe07952350aa59ae
Juliana Villegas <jumavipe@hotmail.com>
2018-11-20 11:18:27 -0500

    agrego mentech

---
+++ mentech/precache-manifest.481a76093db1cabebce1033b265383c0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ad1c1b82fba5912f7e64",
+    "url": "/static/js/main.ad1c1b82.chunk.js"
+  },
+  {
+    "revision": "e02250da0a817af9052d",
+    "url": "/static/js/1.e02250da.chunk.js"
+  },
+  {
+    "revision": "ad1c1b82fba5912f7e64",
+    "url": "/static/css/main.d5e27b53.chunk.css"
+  },
+  {
+    "revision": "e02250da0a817af9052d",
+    "url": "/static/css/1.57c41606.chunk.css"
+  },
+  {
+    "revision": "536d6a9220b4ae4739da056bb3679bff",
+    "url": "/index.html"
+  }
+];
